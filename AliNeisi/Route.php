cff074a84c78bd3a0ecebaa0b49b6ff4795c0ed0
AliNeisi <develop.bandit@gmail.com>
2022-04-28 14:42:24 +0430

    first commit

---
+++ AliNeisi/Route.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace Alineisi;
+
+use ReflectionMethod;
+use RuntimeException;
+
+function routeName($name)
+{
+    global $names;
+    if (isset($names[$name]))
+        return '/' . $names[$name];
+    else
+        return "Route name `$name` is not defined";
+}
+
+class Route
+{
+    protected static $path_to_notfound_page = '';
+
+    protected static $routes = [];
+
+    public static function notFoundPath($path)
+    {
+        self::$path_to_notfound_page = $path;
+    }
+
+    public static function view($url, $view)
+    {
+        self::$routes[self::getRoute($url, 'get')] = ['type' => 'view', 'callback' => $view];
+        return new Methods($url, 'get');
+    }
+
+    public static function get($url, $callback)
+    {
+        self::$routes[self::getRoute($url, 'get')] = ['type' => 'get', 'callback' => $callback];
+        return new Methods($url, 'get');
+    }
+
+    public static function post($url, $callback)
+    {
+        self::$routes[self::getRoute($url, 'post')] = ['type' => 'post', 'callback' => $callback];
+        return new Methods($url, 'post');
+    }
+
+    protected static function getRoute($url, $method)
+    {
+        $method = strtolower($method);
+        $query_string = $_SERVER['QUERY_STRING'] ?? '';
+        if ($query_string == '')
+            return self::getUrl($url) . ".$method";
+        return self::getUrl($url) . "?$query_string.$method";
+    }
+
+    protected static function getUrl($url)
+    {
+        if (strpos($url, '/') === 0) {
+            return substr($url, 1, strlen($url));
+        }
+        return $url;
+    }
+
+    protected static function getRouteIndex($url, $method)
+    {
+        $method = strtolower($method);
+        return self::getUrl($url) . ".$method";
+    }
+
+    public static function dispatch()
+    {
+        $path = self::getUrl($_SERVER['REQUEST_URI']);
+        $method = strtolower($_SERVER['REQUEST_METHOD']);
+        $result = self::$routes[self::getRouteIndex($path, $method)] ?? '';
+        $callback = $result['callback'] ?? '';
+        $type = $result['type'] ?? '';
+        switch ($type) {
+            case 'post':
+            case 'get':
+                if (is_array($callback)) {
+                    $class = $callback[0];
+                    $function = $callback[1];
+                    $reflectionMethod = new ReflectionMethod($class, $function);
+                    if ($reflectionMethod->isStatic())
+                        $result = $callback();
+                    else
+                        $result = (new $class)->$function();
+                } else
+                    $result = $callback();
+                if (is_string($result)) echo $result;
+                break;
+            case
+            'view':
+                self::getView($callback);
+                break;
+            default:
+                self::abort();
+        }
+    }
+
+    protected static function abort()
+    {
+        http_response_code(404);
+        $path = self::$path_to_notfound_page;
+        if ($path == '') echo 'Not Found';
+        else self::getView($path);
+        die;
+    }
+
+    protected static function getView($path)
+    {
+        if (strpos($path, '.') !== false) {
+            $explode = explode('.', $path);
+            $extension = $explode[count($explode) - 1];
+            if ($extension == '') {
+                $path = trim($path, '.');
+                require_once "$path.php";
+            } else require_once $path;
+        } else
+            require_once "$path.php";
+    }
+
+}
+
+class Methods
+{
+    protected $url = '';
+    protected $method = '';
+
+    public function __construct($url, $method)
+    {
+        $this->url = $url;
+        $this->method = $method;
+    }
+
+    public function name($name)
+    {
+        global $names;
+        $names[$name] = $this->getUrl($this->url);
+    }
+
+    protected function getUrl($url)
+    {
+        if (strpos($url, '/') === 0) {
+            return substr($url, 1, strlen($url));
+        }
+        return $url;
+    }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+# router
+# Routing system for PHP
+## Requries PHP >= 7.0
+## Installation
+- `composer require alineisi/router`
+## To get started
+- `require_once __DIR__ . '/vendor/autoload.php;`
+- `use Alineisi\Route;`
+- be sure putting `Route::dispatch();` end of index.php
+## Features
+- ### you can set not found page by using 
+- `Route::notFoundPath($path);`
+- ### set named routes by using name method after routing, for example:
+- ``` Route::get('/', function () { ```
+- ```   return 'Hello World'; ```
+- ``` })->name('index'); ```
+- and to use it:
+- `\Alineisi\routeName('index');`
+- ### you can also returning views by using
+- `Route::view('index');`
+- or
+- `Route::view('index.php');`
